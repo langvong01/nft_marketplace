81f1449eba21acdf6f24372f940d2dedb9d1c630
Dieu <vuvandieu250201@gmail.com>
2022-12-20 17:48:10 +0700

    dieu:finish project

--- components/questions/Question.jsx
+++ components/questions/Question.jsx
@@ -1,6 +1,9 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const Question = () => {
+  const router = useRouter();
   return (
     <>
       <div className="w-[95%] mx-auto mb-14">
@@ -19,33 +22,45 @@ const Question = () => {
         </div>
 
         <div className="questions-bottom mt-4 grid grid-cols-4 justify-between gap-8 ">
-          <div className="category-item cursor-pointer   rounded-lg overflow-hidden shadow-md w-full h-[400px]">
-            <div className="category-image ">
-              <img
-                src="https://opensea.io/static/images/learn-center//how-to-create-nft.png"
-                alt="arts"
-                className="w-full  h-[250px] object-cover"
-              />
+          <a
+            target={`_blank`}
+            href={`https://opensea.io/learn/what-are-nfts`}
+            className="hover:text-inherit"
+          >
+            <div className="category-item cursor-pointer   rounded-lg overflow-hidden shadow-md w-full h-[400px]">
+              <div className="category-image ">
+                <img
+                  src="https://opensea.io/static/images/learn-center//how-to-create-nft.png"
+                  alt="arts"
+                  className="w-full  h-[250px] object-cover"
+                />
+              </div>
+              <div className="category-text text-center flex justify-center items-center h-[150px]">
+                <h2 className="p-8 text-2xl text-center">What is an NFT?</h2>
+              </div>
             </div>
-            <div className="category-text text-center flex justify-center items-center h-[150px]">
-              <h2 className="p-8 text-2xl text-center">
-                How to create an NFT?
-              </h2>
-            </div>
-          </div>
+          </a>
 
-          <div className="category-item cursor-pointer   rounded-lg overflow-hidden shadow-md w-full h-[400px]">
-            <div className="category-image ">
-              <img
-                src="https://opensea.io/static/images/learn-center//how-to-sell-nft.png"
-                alt="arts"
-                className="w-full  h-[250px] object-cover"
-              />
+          <a
+            href="https://opensea.io/learn/what-is-crypto-wallet"
+            target="_blank"
+            className="hover:text-inherit"
+          >
+            <div className="category-item cursor-pointer   rounded-lg overflow-hidden shadow-md w-full h-[400px]">
+              <div className="category-image ">
+                <img
+                  src="https://opensea.io/static/images/learn-center//how-to-sell-nft.png"
+                  alt="arts"
+                  className="w-full  h-[250px] object-cover"
+                />
+              </div>
+              <div className="category-text text-center flex justify-center items-center h-[150px]">
+                <h2 className="p-8 text-2xl text-center">
+                  What is a crypto wallet?
+                </h2>
+              </div>
             </div>
-            <div className="category-text text-center flex justify-center items-center h-[150px]">
-              <h2 className="p-8 text-2xl text-center">How to sell an NFT?</h2>
-            </div>
-          </div>
+          </a>
 
           <div className="category-item cursor-pointer   rounded-lg overflow-hidden shadow-md w-full h-[400px]">
             <div className="category-image ">
@@ -63,7 +78,7 @@ const Question = () => {
           <div className="category-item cursor-pointer   rounded-lg overflow-hidden shadow-md w-full h-[400px]">
             <div className="category-image ">
               <img
-                src="https://opensea.io/static/images/learn-center//what-is-minting.png"
+                src="https://opensea.io/static/images/learn-center//what-are-gas-fees.png"
                 alt="arts"
                 className="w-full h-[250px] object-cover "
               />
